34ee2d63c306cf284e0926d927f53869af416548
Mihir Bindal <59363084+MihirBindal@users.noreply.github.com>
2020-06-23 15:46:58 +0530

    changed to linear model

--- predict.py
+++ predict.py
@@ -1,16 +1,15 @@
 from sklearn.datasets import load_boston
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 import pickle
 
 X, y = load_boston(return_X_y=True)
 X = pd.DataFrame(data=X, columns=load_boston().feature_names)
 y = pd.DataFrame(data=y, columns=["Price"])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=420)
-rf = RandomForestRegressor()
-best_rf = RandomForestRegressor(max_depth=7, n_estimators=70, max_features=7, min_samples_leaf=1)
-best_rf.fit(X_train, y_train)
-y_predicted = best_rf.predict(X_train)
+rf = LinearRegression()
+rf.fit(X_train, y_train)
+y_predicted = rf.predict(X_train)
 filename = 'finalized_model.pkl'
-pickle.dump(best_rf, open(filename, 'wb'))
+pickle.dump(rf, open(filename, 'wb'))
